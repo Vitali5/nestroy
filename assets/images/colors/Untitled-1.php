4913e12013dd058a0f2d0d30db408e1434be650d
hs <rom541997@gmail.com>
2018-01-26 16:50:14 +0700

    Initial committ

---
+++ assets/images/colors/Untitled-1.php
@@ -0,0 +1,27 @@
+﻿<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0">
+  <title>Provedal | Спасибо</title>
+  <link rel="stylesheet" href="css/third.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="fonts/stylesheet.css">
+</head>
+<body class="thanks-body"
+  <section class="entry section" id="entry">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-3"></div>
+        <div class="col-xs-6">
+          <div class="form-box text-center">
+            <h1>Спасибо!</h1>
+            <p>Наши специалисты свяжутся с вами ближайшее время.</p>
+            <p>Желаем вам удачи!</p>
+          </div>
+        </div>
+        <div class="col-xs-3"></div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
